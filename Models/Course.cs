d8952a5f10ee13abe939548e5cc81f9314c9f3aa
LegioinOfToasters <raze620@gmail.com>
2020-02-27 15:01:28 -0500

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        [Required]
+        [ConcurrencyCheck]
+        [MaxLength(24, ErrorMessage = "The maximum length is 24 characters")]
+        [MinLength(5, ErrorMessage = "The minimum length is 5 characters")]
+        [Index(IsUnique = true)]
+        public string Title { get; set; }
+        [Range(1, 6)]
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
